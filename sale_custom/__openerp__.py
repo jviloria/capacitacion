564007b5dd22a5fad929f1c99771794c54835674
John W. Viloria Amaris <john.viloria.amaris@gmail.com>
2017-02-11 09:36:57 -0500

    [UPD] sale_custom: se actualiza archivo __openerp__.py

---
+++ sale_custom/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':'Modificaciones al módulo de Ventas',
+    'author':'John Viloria',
+    'category' : 'Ventas',
+    'description' : """
+MODIFICACIONES AL MODULO DE VENTAS
+==================================
+
+Mi primer módulo de Odoo
+
+    """,
+    'depends' : ['sale',],
+    'data':[],
+    'installable': True
+
+}
